bc62c2f274fd8d30faf759cfd407cf3e8796ec2d
Siam19395 <siamchowdhury88888@gmail.com>
2024-06-27 09:06:10 +0600

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    API_ID = os.environ.get("API_ID", "28986163")
+    API_HASH = os.environ.get("API_HASH", "07225d0de9bee70666517315d2174171")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7366003305:AAH50pdBo-hlNyJ-UdrbG_fl7o1XY-Yohqc") 
+    BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot") 
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://siamchowdhury88888:4A9JTKbenH7AKfdF@siam80.9lqthzx.mongodb.net/?retryWrites=true&w=majority")
+    DB_NAME = os.environ.get("DB_NAME", "forward12")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6941649360').split()]
 
 
 class temp(object): 
